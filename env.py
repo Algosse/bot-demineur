a05b9fe26ee205fbe3aca0f15195d3236f6911f4
= <gosset.albangmail.com>
2022-04-24 22:21:33 +0200

    upgrades and training

--- env.py
+++ env.py
@@ -3,17 +3,24 @@ import pyautogui
 import numpy as np
 import os
 import torch
+import torchvision.transforms as T
 
 from window import WindowInterface
 
 class DemineurInterface():
     
-    def __init__(self):
+    def __init__(self, rows = 16, cols = 30):
         
         self.windowInterface = WindowInterface("Démineur")
         self.windowInterface.move_and_resize(w=500,h=320)
-        self.grid = Grid()
+        self.grid = Grid(rows, cols)
         self.action_space_nb = self.grid.action_space_nb
+        self.victories = 0
+
+        self.transform = T.Compose([
+            T.ToTensor(),
+            T.Normalize(7, 7)
+        ])
     
     def grab_image(self):
         
@@ -24,48 +31,64 @@ class DemineurInterface():
     
     def reset(self):
         self.grid.reset_grid()
+        #print("reset")
+        
         
     
     def get_state(self):
         
         img = self.grab_image()
         grid = self.grid.extract_from_image(img)
-         
+
+        return torch.unsqueeze(self.transform(grid), 0).to(torch.float32)
+
         return torch.unsqueeze(torch.unsqueeze(torch.Tensor(grid), 0), 0)        
     
     def send_control(self, x, y):
         pyautogui.click(x, y)
         
-    def step(self, index):
+    def step(self, index, cpt = 1):
                
         i = index // self.grid.cols
         j = index % self.grid.cols
+
+        x, y = self.grid.click_pos[i,j]
         
         if not self.grid.is_free(i,j):
             #print(f"Case {i},{j} is used")
+            pyautogui.moveTo(x, y)
             return 0, False # Reward, Done
         
-        x, y = self.grid.click_pos[i,j]
+        isolated = self.grid.is_isolated(i,j)
+        
         
         self.send_control(x, y)
         
         img = self.grab_image()
+
+        # for debugging
+        #cv2.imwrite("output/{}.png".format(cpt), img)
+
         done = self.grid.is_done(img)
         
         #print(f"Click Case {i},{j}. done:", done)
         
         if done == 0:
-            # If not over, since the action was clicking on an empty case, it's not over 
+            # If not over, since the action was clicking on an empty case, it's not over
+
+            if isolated:
+                # Try to influence the model to click near already known cases
+                return .7, False
+
             return 1, False  
         elif done == 1:
             # Victory
+            self.victories += 1
             return 1, True
         else:
             # Click on a bomb
             return 0, True
         
-
-
 
 class Grid():
     
@@ -81,7 +104,14 @@ class Grid():
         self.status_width = 26
         self.status_height = 26
         self.status_i = 15
-        self.status_j = 239
+
+        assert self.cols in [9, 16, 30], "Unknown Pattern"
+        if self.cols == 9:
+            self.status_j = 71
+        elif self.cols == 16:
+            self.status_j = 127
+        else:
+            self.status_j = 239
         
         self.window_offset_i = 46
         self.window_offset_j = 1
@@ -102,10 +132,13 @@ class Grid():
         
     def reset_grid(self):
         # Twice to get the focus
+        pyautogui.press("enter")
+        pyautogui.press("enter")
         pyautogui.click(self.window_offset_j + self.status_j + self.status_height // 2, self.window_offset_i + self.status_i + self.status_width // 2)
-        pyautogui.click(self.window_offset_j + self.status_j + self.status_height // 2, self.window_offset_i + self.status_i + self.status_width // 2)
+        #pyautogui.click(self.window_offset_j + self.status_j + self.status_height // 2, self.window_offset_i + self.status_i + self.status_width // 2)
+        #pyautogui.press('F2')
         
-        self.grid = np.zeros((self.rows, self.cols)) + self.cases_labels.index("raw") + 1
+        self.grid = np.zeros((self.rows, self.cols)) + self.cases_labels.index("raw")
         self.done = 0
 
     def load_cases(self):
@@ -162,14 +195,25 @@ class Grid():
         return np.argmin(diff)
 
     def is_done(self, img):
-        
-        status_extract = img[self.status_i : self.status_i + self.status_height, self.status_j: self.status_j + self.status_width]
-        
-        result = self.get_matching_template(status_extract, self.status_templates)
-        
-        if result == self.status_labels.index("continue"):
+                
+        img = img.astype(np.float32)
+
+        maxi = 0
+        iRef = 0
+
+        for i in range(len(self.status_templates)):
+
+            template = self.status_templates[i].astype(np.float32)
+
+            res = cv2.matchTemplate(img, template, cv2.TM_CCOEFF)
+
+            if res.max() > maxi:
+                maxi = res.max()
+                iRef = i
+
+        if iRef == self.status_labels.index("continue"):
             return 0
-        elif result == self.status_labels.index("victory"):
+        elif iRef == self.status_labels.index("victory"):
             return 1
         else:
             return 2
@@ -178,3 +222,18 @@ class Grid():
     def is_free(self, i, j):
         
         return self.grid[i,j] == self.cases_labels.index("raw")
+
+    def is_isolated(self, i, j):
+
+        mask = np.zeros((3,3)) + self.cases_labels.index("raw")
+
+        grid_pad = np.copy(self.grid)
+        grid_pad = np.pad(grid_pad, 1, constant_values=self.cases_labels.index("raw"))
+        return (grid_pad[i:i+3, j:j+3] == mask).all()
+    
+if __name__ == "__main__":
+
+    env = DemineurInterface(9, 9)
+
+    img = env.grab_image()
+    print(env.grid.is_done(img))
--- model.py
+++ model.py
@@ -22,3 +22,25 @@ class BotDemineur(nn.Module):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         return self.head(x.view(x.size(0), -1))
+    
+
+class BotDemineurV2(nn.Module):
+    
+    def __init__(self, rows = 16, cols = 30, outputs = 480):
+        
+        super(BotDemineur, self).__init__()
+        
+        self.conv1 = nn.Conv2d(1, 3, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(3)
+        self.conv2 = nn.Conv2d(3, 3, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(3)
+        
+        # Due to no padding, the size of the output of conv2 is w-2 x h-2
+        linear_output_size = (rows - 2) * (cols - 2) * 3
+        self.head = nn.Linear(linear_output_size, outputs)
+        
+    def forward(self, x):
+        
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        return self.head(x.view(x.size(0), -1))
